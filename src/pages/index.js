9e702386fcfddb437f3c6d2a5e4ab7e5cca2eab1
Aldo Arya Saka Mukti <60910170+aldoarya@users.noreply.github.com>
2023-02-26 15:38:02 +0700

    chore: update template

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Nama] | [Lab Informatika]
+            [Nama] | [NIU]
           </h5>
         </div>
       </main>
